42409f5d14ba96f2f479c09c67b2d6f2072e8c45
Fahad Khan <fk.fahad96@gmail.com>
2025-02-17 17:28:49 +0500

    feat(fizz-buzz): add solution

---
+++ fizz-buzz-problem/main.go
@@ -0,0 +1,42 @@
+// Problem Statement
+// You need to print numbers from 1 to n, but with the following conditions:
+
+// If a number is divisible by 3, print "Fizz" instead of the number.
+// If a number is divisible by 5, print "Buzz" instead of the number.
+// If a number is divisible by both 3 and 5 (i.e., 15), print "FizzBuzz" instead of the number.
+// If the number is not divisible by 3 or 5, print the number itself.
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	output := fizzBuzz(15)
+	fmt.Println("result: ", output)
+}
+
+// Time Complexity O(n) ; Space Complexity O(n)
+func fizzBuzz(n int) []string {
+	var output []string
+	for i := 1; i <= n; i++ {
+		str := ""
+
+		if i%3 == 0 {
+			str += "Fizz"
+		}
+
+		if i%5 == 0 {
+			str += "Buzz"
+		}
+
+		if str == "" {
+			// strconv.Itoa used for integer to string conversion
+			str += strconv.Itoa(i)
+		}
+		output = append(output, str)
+	}
+	return output
+}
