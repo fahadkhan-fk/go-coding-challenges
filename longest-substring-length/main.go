70992168fbb0e337a064301a5b9fae9537082f76
Fahad Khan <fk.fahad96@gmail.com>
2025-02-21 18:23:42 +0500

    feat(longest-substring-length): add solution

---
+++ longest-substring-length/main.go
@@ -0,0 +1,35 @@
+// Problem Statement
+// Given a string, and you need to find the length of the longest substring
+// that contains no repeating characters.
+// Example:
+// s = "abcabcbb"
+// Substrings Without Repeating Characters: "abc" -> Length 3
+
+package main
+
+import "fmt"
+
+func main() {
+	len := lengthOfLongestSubstring("abcabcbb")
+	fmt.Println("longest substring length: ", len)
+}
+
+func lengthOfLongestSubstring(s string) int {
+	charMap := make(map[rune]int)
+	maxLength := 0
+	start := 0 // left boundary of the sliding window
+	for end, value := range s {
+		// if character already exists in the map AND is within the window, move `start`
+		if index, exists := charMap[value]; exists && index >= start {
+			start = index + 1
+		}
+		// store/update the last seen position of the character
+		charMap[value] = end
+		// update maxLength
+		currentLength := end - start + 1
+		if currentLength > maxLength {
+			maxLength = currentLength
+		}
+	}
+	return maxLength
+}
