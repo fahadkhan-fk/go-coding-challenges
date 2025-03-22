b2fa5e4d7ff2f4395157aba9422b73299f295934
Fahad Khan <fk.fahad96@gmail.com>
2025-03-22 20:48:31 +0500

    feat(fizz-buzz): add clean solution

--- fizz-buzz-problem/main.go
+++ fizz-buzz-problem/main.go
@@ -40,3 +40,20 @@ func fizzBuzz(n int) []string {
 	}
 	return output
 }
+
+// Alternative clean approach
+// func fizzBuzz(n int) []string {
+// 	arr := []string{}
+// 	for i := 1; i <= n; i++ {
+// 		if i%3 == 0 && i%5 == 0 {
+// 			arr = append(arr, "FizzBuzz")
+// 		} else if i%3 == 0 {
+// 			arr = append(arr, "Fizz")
+// 		} else if i%5 == 0 {
+// 			arr = append(arr, "Buzz")
+// 		} else {
+// 			arr = append(arr, strconv.Itoa(i))
+// 		}
+// 	}
+// 	return arr
+// }
